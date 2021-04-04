b17ef7197d5fdb132c2b8007c96ae1381bc41a3e
BreySean <breytonernstzen44@gmail.com>
2021-04-04 22:32:00 +0200

    Two methods added by Dummy

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -6,4 +6,11 @@ public class Calculator {
      public int addMultiplication(int x,int y){
          return x * y;
      }
+     //Team member edited 2 methods
+     public double addSubstraction(double  a,double b){
+         return a - b;
+     }
+     public double addDiv(double x,double y){
+         return x / y;
+     }
 }
