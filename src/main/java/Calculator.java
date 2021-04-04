786e5c8e056303f4f68730b23b76a8504f77e953
breytonE <breytonseanernstzen1224@gmail.com>
2021-04-04 21:55:07 +0200

    all files added

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,9 @@
+
+public class Calculator {
+     public int addSumm(int a,int b){
+         return a + b;
+     }
+     public int addMultiplication(int x,int y){
+         return x * y;
+     }
+}
